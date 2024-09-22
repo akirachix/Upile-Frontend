7563888f6a3c3da5effde1764cdff31467593536
Esther-Shiamagovi <shiamagoviesther@gmail.com>
2024-09-22 12:34:17 +0300

    resolved conflicts

--- lostlocate/src/app/page.tsx
+++ lostlocate/src/app/page.tsx
@@ -2,17 +2,12 @@ import Layout from "./Layout";
 import React from "react";
 import Dashboard from "./Dashboard";
 
-
 export default function Home() {
   return (
     <div> 
       <Layout>
-        
+        <Dashboard /> 
       </Layout>
-      <Dashboard/>
-
-       <main>            
-        </main>
     </div>
-
-  )}
+  );
+}
